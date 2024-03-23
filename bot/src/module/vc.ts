1c8445467db532aef27d04801850f409172843a1
ruther2525 <ruther2525@gmail.com>
2024-03-24 00:32:07 +0900

    ✨add vc channel counter

--- bot/src/module/vc.ts
+++ bot/src/module/vc.ts
@@ -4,65 +4,153 @@ import { Client } from "discord.js";
 const VCChannels = [];
 
 export const VC = (client: Client) => {
+    client.on('channelCreate', async (channel) => {
+        if (channel?.isVoiceBased()) {
+            console.log(`${channel.name} has been created.`);
+            VCChannels.push(channel);
+
+            await prisma.vCChannel.create({
+                data: {
+                    channel_id: channel.id,
+                    channel_name: channel.name,
+                    category_name: channel.parent?.name ?? null,
+                }
+            });
+        }
+    });
+
     client.on('voiceStateUpdate', async (oldState, newState) => {
+        const _user = await prisma.user.findFirst({
+            where: {
+                discord_id: newState.member?.id
+            }
+        });
+        const _channel = await prisma.vCChannel.findFirst({
+            where: {
+                channel_id: newState.channelId ?? oldState.channelId ?? ''
+            }
+        });
+
+
+        // チャンネル更新
+        if (_channel && (newState.channel || oldState.channel)) {
+            // 1. チャンネルが初めて使用された場合、初めて使用された時間をセット&初めて使用されたメンバー数をセット
+            if (
+                _channel.vc_first_join_time === null
+                && oldState.channelId === null && newState.channelId !== null
+            ) {
+                await prisma.vCChannel.update({
+                    where: {
+                        channel_id: newState.channelId ?? oldState.channelId ?? ''
+                    },
+                    data: {
+                        vc_first_join_time: new Date(),
+                        vc_total_member: newState.channel?.members?.size ?? null,
+                    }
+                });
+
+            // 2. チャンネルが既に使用されている場合かつ、人数が1人以上いて、メンバー数がデータベースよりも多い場合、メンバー数を更新
+            } else if (
+                (newState.channel?.members?.size ?? 0) > (_channel.vc_total_member ?? 1)) {
+                await prisma.vCChannel.update({
+                    where: {
+                        channel_id: newState.channelId ?? oldState.channelId ?? ''
+                    },
+                    data: {
+                        vc_total_member: newState.channel?.members?.size ?? null,
+                    }
+                });
+
+            // 3. チャンネルが既に使用されている場合かつ、誰もいなくなった場合、最後に使用された時間をセット&トータル時間を更新
+            } else if (
+                _channel.vc_total_member !== null
+                && _channel.vc_first_join_time !== null
+                && oldState.channelId !== null && newState.channelId === null
+                && oldState.channelId === _channel.channel_id
+            ) {
+                await prisma.vCChannel.update({
+                    where: {
+                        channel_id: newState.channelId ?? oldState.channelId ?? ''
+                    },
+                    data: {
+                        vc_last_leave_time: new Date(),
+                        vc_total_sec: (_channel.vc_total_sec ?? 0) + Math.floor((new Date().getTime() - _channel.vc_first_join_time.getTime()) / 1000)
+                    }
+                });
+            }
+
+        // チャンネルがデータベースに存在しない場合
+        } else {
+            await prisma.vCChannel.create({
+                data: {
+                    channel_id: newState.channelId ?? '',
+                    channel_name: newState.channel?.name ?? '',
+                    category_name: newState.channel?.parent?.name ?? null,
+
+                    vc_first_join_time: ((newState.channel?.members?.size ?? 0) > 0) ? new Date() : null,
+                    vc_total_member: newState.channel?.members?.size ?? null,
+                }
+            });
+            console.log(`- ${newState.channel?.name} is not in the database.`);
+        }
+
+
+        // チャンネル参加
         if (oldState.channelId === null && newState.channelId !== null) {
             console.log(`${newState.member?.user.tag} joined ${newState.channel?.name}`);
 
-            const _user = await prisma.user.findFirst({
-                where: {
-                    discord_id: newState.member?.id
-                }
-            });
-
+            // ユーザーがデータベースに存在する場合
             if (_user) {
+                // ユーザー更新
                 await prisma.user.update({
                     where: {
                         discord_id: _user.discord_id
                     },
                     data: {
+                        discord_name: newState.member?.user.tag,
+                        discord_avatar: newState.member?.user.avatarURL() ?? null,
                         vc_last_join_time: new Date(),
                         vc_last_join_channel: newState.channelId
                     }
                 });
-                console.log(`- User ${newState.member?.user.tag} is in the database.`);
+
+            // ユーザーがデータベースに存在しない場合
             } else if (newState.member) {
+                // ユーザー追加
                 await prisma.user.create({
                     data: {
                         discord_id: newState.member.id,
+                        discord_name: newState.member.user.tag,
+                        discord_avatar: newState.member.user.avatarURL() ?? null,
                         vc_last_join_time: new Date(),
                         vc_last_join_channel: newState.channelId
                     }
                 });
                 console.log(`- User ${newState.member?.user.tag} is not in the database.`);
             }
+
+        // チャンネル退出
         } else if (newState.channelId === null && oldState.channelId !== null) {
             console.log(`${oldState.member?.user.tag} left ${oldState.channel?.name}`);
 
-            const _user = await prisma.user.findFirst({
-                where: {
-                    discord_id: oldState.member?.id
-                }
-            });
-
+            // ユーザーがデータベースに存在する場合
             if (_user && _user.vc_last_join_time) {
                 await prisma.user.update({
                     where: {
                         discord_id: _user.discord_id
                     },
                     data: {
+                        discord_name: oldState.member?.user.tag,
+                        discord_avatar: oldState.member?.user.avatarURL() ?? null,
                         vc_last_leave_time: new Date(),
                         vc_total_sec: (_user.vc_total_sec ?? 0) + Math.floor((new Date().getTime() - _user.vc_last_join_time.getTime()) / 1000)
                     }
                 });
             }
+
+        // チャンネル移動
         } else if (oldState.channelId !== null && newState.channelId !== null && oldState.channelId !== newState.channelId){
             console.log(`${oldState.member?.user.tag} moved from ${oldState.channel?.name} to ${newState.channel?.name}`);
-
-            const _user = await prisma.user.findFirst({
-                where: {
-                    discord_id: oldState.member?.id
-                }
-            });
 
             if (_user && _user.vc_last_join_time && _user.vc_last_join_channel) {
                 await prisma.user.update({
@@ -70,23 +158,39 @@ export const VC = (client: Client) => {
                         discord_id: _user.discord_id
                     },
                     data: {
+                        discord_name: newState.member?.user.tag,
+                        discord_avatar: newState.member?.user.avatarURL() ?? null,
                         vc_last_join_channel: newState.channelId,
                     }
                 });
             }
+        // その他ミュートとかのイベント
         } else {
+            // サーバーミュート
             if (oldState.serverMute !== newState.serverMute) {
                 console.log(`${newState.member?.user.tag} changed serverMute to ${newState.serverMute}`);
+
+            // サーバースピーカーミュート
             } else if (oldState.serverDeaf !== newState.serverDeaf) {
                 console.log(`${newState.member?.user.tag} changed serverDeaf to ${newState.serverDeaf}`);
+
+            // セルフミュート
             } else if (oldState.selfMute !== newState.selfMute) {
                 console.log(`${newState.member?.user.tag} changed selfMute to ${newState.selfMute}`);
+
+            // セルフスピーカーミュート
             } else if (oldState.selfDeaf !== newState.selfDeaf) {
                 console.log(`${newState.member?.user.tag} changed selfDeaf to ${newState.selfDeaf}`);
+
+            // カメラ配信
             } else if (oldState.selfVideo !== newState.selfVideo) {
                 console.log(`${newState.member?.user.tag} changed selfVideo to ${newState.selfVideo}`);
+
+            // 配信
             } else if (oldState.streaming !== newState.streaming) {
                 console.log(`${newState.member?.user.tag} changed streaming to ${newState.streaming}`);
+
+            // その他よくわからないイベント
             } else {
                 console.log(`${newState.member?.user.tag} changed something else`);
             }
@@ -103,12 +207,90 @@ export const VC = (client: Client) => {
         const channels = await guild.channels.fetch();
 
         const joinnedUser: string[] = [];
-        channels.forEach(channel => {
+        channels.forEach(async channel => {
             if (channel?.isVoiceBased()) {
-                console.group(`Found voice channel: ${channel.name}`);
                 VCChannels.push(channel);
+                const _channel = await prisma.vCChannel.findFirst({
+                    where: {
+                        channel_id: channel.id
+                    }
+                });
+
+                // Botがダウンしてたときの回復動作
+
+                // チャンネル更新
+                // チャンネルがデータベースに存在する場合
+                if (_channel) {
+                    console.log(`- ${channel.name} is in the database.`);
+
+                    // 1. チャンネルが初めて使用された場合、初めて使用された時間をセット&初めて使用されたメンバー数をセット
+                    if (
+                        _channel.vc_first_join_time === null
+                        && channel.members.size !== 0
+                    ) {
+                        await prisma.vCChannel.update({
+                            where: {
+                                channel_id: channel.id
+                            },
+                            data: {
+                                vc_first_join_time: new Date(),
+                                vc_total_member: channel.members.size,
+                            }
+                        });
+
+                    // 2. チャンネルが既に使用されている場合かつ、人数が1人以上いて、メンバー数がデータベースよりも多い場合、メンバー数を更新
+                    } else if (
+                        _channel.vc_total_member !== null
+                        && channel.members.size > _channel.vc_total_member
+                        && _channel.vc_first_join_time !== null
+                        && (
+                            _channel.vc_last_leave_time === null
+                            || _channel.vc_first_join_time > _channel.vc_last_leave_time
+                        )
+                    ) {
+                        await prisma.vCChannel.update({
+                            where: {
+                                channel_id: channel.id
+                            },
+                            data: {
+                                vc_total_member: channel.members.size,
+                            }
+                        });
+
+                    // 3. チャンネルが既に使用されている場合かつ、誰もいなくなった場合、最後に使用された時間をセット&トータル時間を更新
+                    } else if (
+                        _channel.vc_total_member !== null
+                        && channel.members.size === 0
+                        && _channel.vc_first_join_time !== null
+                        && _channel.vc_first_join_time > (_channel.vc_last_leave_time ?? new Date())
+                    ) {
+                        await prisma.vCChannel.update({
+                            where: {
+                                channel_id: channel.id
+                            },
+                            data: {
+                                vc_last_leave_time: new Date(),
+                                vc_total_sec: (_channel.vc_total_sec ?? 0) + Math.floor((new Date().getTime() - (_channel.vc_first_join_time?.getTime() ?? new Date().getTime())) / 1000)
+                            }
+                        });
+                    }
+
+                // チャンネルがデータベースに存在しない場合
+                } else {
+                    console.log(`- ${channel.name} is not in the database.`);
+                    await prisma.vCChannel.create({
+                        data: {
+                            channel_id: channel.id,
+                            channel_name: channel.name,
+                            category_name: channel.parent?.name ?? null,
+                        }
+                    });
+                }
+
+                // メンバー更新
+                // チャンネルに誰かがいる場合
                 if (channel.members.size > 0) {
-                    console.log(`Members in ${channel.name}:`);
+                    console.group(`Members in ${channel.name}:`);
                     channel.members.forEach(async member => {
 
                         joinnedUser.push(member.id);
@@ -117,7 +299,6 @@ export const VC = (client: Client) => {
                         if (_user) {
                             console.log(`- ${member.user.tag} is in the database.`);
 
-                            // Botがダウンしてたときの回復動作
                             // 1. ユーザーが最後に参加した時間がnullの場合、現在の時間をセット
                             if (_user.vc_last_join_time === null
                                 || (
@@ -130,6 +311,8 @@ export const VC = (client: Client) => {
                                         discord_id: _user.discord_id
                                     },
                                     data: {
+                                        discord_name: member.user.tag,
+                                        discord_avatar: member.user.avatarURL() ?? null,
                                         vc_last_join_time: new Date(),
                                     }
                                 });
@@ -144,8 +327,10 @@ export const VC = (client: Client) => {
                                         discord_id: _user.discord_id
                                     },
                                     data: {
+                                        discord_name: member.user.tag,
+                                        discord_avatar: member.user.avatarURL() ?? null,
                                         vc_last_join_time: new Date(),
-                                        vc_last_join_channel: channel.id
+                                        vc_last_join_channel: channel.id,
                                     }
                                 });
                             }
@@ -154,20 +339,21 @@ export const VC = (client: Client) => {
                             await prisma.user.create({
                                 data: {
                                     discord_id: member.id,
+                                    discord_name: member.user.tag,
+                                    discord_avatar: member.user.avatarURL() ?? null,
                                     vc_last_join_time: new Date(),
-                                    vc_last_join_channel: channel.id
+                                    vc_last_join_channel: channel.id,
                                 }
                             });
                             console.log(`- ${member.user.tag} is not in the database.`);
                             // それ以外は何もしない
                         }
                     });
+                    console.groupEnd();
                 }
-                console.groupEnd();
             }
         });
 
-        // Botがダウンしてたときの回復動作2
 
         // 3. ユーザーが最後に退出した時間よりも最後に参加した時間が新しいのに、VCに参加していない場合、最後に退出した時間を現在の時間にセット
         dbUser.forEach(async user => {
